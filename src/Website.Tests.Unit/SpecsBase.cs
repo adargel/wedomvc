103e473a21f2db79355b532803af73e03e9b1ce5
Aaron Dargel <aaron@nervousyak.com>
2010-11-03 12:15:28 -0500

    Added appstartuprunner and specs.

---
+++ src/Website.Tests.Unit/SpecsBase.cs
@@ -0,0 +1,12 @@
+﻿using Rhino.Mocks;
+
+namespace Website.Tests.Unit.StartupSpecs
+{
+    public class SpecsBase
+    {
+        protected static T M<T>() where T: class
+        {
+            return MockRepository.GenerateMock<T>();
+        }
+    }
+}
---
+++ src/Website/Startup/AppStartupRunner.cs
@@ -0,0 +1,18 @@
+﻿namespace Website.Startup
+{
+    public class AppStartupRunner
+    {
+        readonly IRunOnAppStartup[] _needsToRunOnStartup;
+
+        public AppStartupRunner(IRunOnAppStartup[] needsToRunOnStartup)
+        {
+            _needsToRunOnStartup = needsToRunOnStartup;
+        }
+
+        public void Run()
+        {
+            foreach(var instance in _needsToRunOnStartup)
+                instance.Run();
+        }
+    }
+}
