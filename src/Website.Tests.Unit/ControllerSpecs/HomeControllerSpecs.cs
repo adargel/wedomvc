79ccd5d38e8c871a06919545419b076f0dbb0358
Aaron Dargel <aaron@nervousyak.com>
2010-11-03 16:51:49 -0500

    Added some made up specs to the home controller specs.

--- src/Website.Tests.Unit/ControllerSpecs/HomeControllerSpecs.cs
+++ src/Website.Tests.Unit/ControllerSpecs/HomeControllerSpecs.cs
@@ -1,7 +1,19 @@
+using Machine.Specifications;
+
 namespace Website.Tests.Unit.ControllerSpecs
 {
     public class HomeControllerSpecs
     {
         
+    }
+
+    public class When_getting_the_index_view_with_categories
+    {
+        It sets_the_view_model_to_all_of_the_categories;
+    }
+
+    public class When_getting_the_index_view_without
+    {
+        It redirects_the_user_to_the_add_categories_page;
     }
 }
