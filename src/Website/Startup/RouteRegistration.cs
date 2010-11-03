816791cb4d99a058f7184ede15878682cf5ee86b
Aaron Dargel <aaron@nervousyak.com>
2010-11-03 12:27:55 -0500

    Added routes registration to run on app startup.

---
+++ src/Website/Startup/RouteRegistration.cs
@@ -0,0 +1,21 @@
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Website.Startup
+{
+    public class RouteRegistration : IRunOnAppStartup
+    {
+        public void Run()
+        {
+            var routes = RouteTable.Routes;
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+
+        }
+    }
+}
