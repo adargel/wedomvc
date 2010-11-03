4fa5e2b0159610cfb6a019896649400271f69c3b
Aaron Dargel <aaron@nervousyak.com>
2010-11-03 12:29:13 -0500

    Cleaned up Global.asax file.

--- src/Website/Global.asax.cs
+++ src/Website/Global.asax.cs
@@ -1,30 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Routing;
-using StructureMap;
+﻿using StructureMap;
 using Website.Startup;
 
 namespace Website
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
-
     public class MvcApplication : System.Web.HttpApplication
     {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-        }
-
         protected void Application_Start()
         {
             StructureMapInitializer.Initialize();
             ObjectFactory.GetInstance<AppStartupRunner>().Run();
-
-            RegisterRoutes(RouteTable.Routes);
-
         }
     }
 }
