c28a370d66e05713db83c17b76e1810bb7af2262
Aaron Dargel <aaron@nervousyak.com>
2010-11-03 12:20:51 -0500

    Added injecting controller factory and run on app startup instance to configure it.

---
+++ src/Website/Controllers/ControllerFactorySetup.cs
@@ -0,0 +1,12 @@
+﻿using Website.Startup;
+
+namespace Website.Controllers
+{
+    public class ControllerFactorySetup : IRunOnAppStartup
+    {
+        public void Run()
+        {
+            System.Web.Mvc.ControllerBuilder.Current.SetControllerFactory(new InjectingControllerFactory());
+        }
+    }
+}
