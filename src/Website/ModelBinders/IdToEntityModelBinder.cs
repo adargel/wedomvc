76efd78d8141e566e3f7750339220cfbc2adffd0
Aaron Dargel <aaron@nervousyak.com>
2010-11-03 18:04:35 -0500

    Updated entity model binder exceptions.

--- src/Website/ModelBinders/IdToEntityModelBinder.cs
+++ src/Website/ModelBinders/IdToEntityModelBinder.cs
@@ -10,14 +10,20 @@ namespace Website.ModelBinders
     {
         public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
         {
-            var idString = controllerContext.RouteData.Values["id"].ToString();
-            
+            var idValue = controllerContext.RouteData.Values["id"];
+            if(idValue == null)
+                throw new InvalidOperationException(
+                    string.Format("Cannot bind to entity {0} because no id was found in the route data values collection.", 
+                    typeof(T).Name));
+
             int id;
-            if (int.TryParse(idString, out id))
+            if (int.TryParse(idValue.ToString(), out id))
             {
                 return ObjectFactory.GetInstance<IRepository<T>>().GetById(id);
             }
-            throw new InvalidOperationException("No integer value with name 'id' was found in the route data values collection.");
+            throw new InvalidOperationException(string.Format(
+                "Cannot bind to {0} because no integer value with name 'id' was found in the route data values collection.", 
+                typeof(T).Name));
         }
 
         public bool IsMatch(Type modelType)
